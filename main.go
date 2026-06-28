02b38b99e7e8a99cbc5da7c6059699a6f537b998
Maxim Motyshen <m.motyshen@gmail.com>
2026-06-28 07:07:10 +0300

    fix: add main

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"github.com/m-ocean-it/gocoen/analyzer"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
 
+func main() {
+	singlechecker.Main(analyzer.NewAnalyzer())
 }
